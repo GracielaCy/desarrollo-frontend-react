ea764ab71d9735086343cc2b2dd7750d6626cff3
GracielaCy <gracielacayo34@gmail.com>
2024-08-22 18:55:09 -0400

    All project

---
+++ src/screens/Product.jsx
@@ -0,0 +1,11 @@
+import Home from './Products/_componentes/Home'
+import FilterProductTable from './Products/_componentes/FilterProductTable'
+const Product = () => {
+    return (
+        <>
+            <FilterProductTable />
+        </>
+    );
+}
+
+export default Product;
